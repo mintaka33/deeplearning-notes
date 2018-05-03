c012845ac589f5180c0ecc894886e814cc697b42
mintaka33 <mintaka33@outlook.com>
2018-05-03 09:45:26 +0800

    refinement

--- opencv_caffe_image_classifier/opencv_image_classifier.py
+++ opencv_caffe_image_classifier/opencv_image_classifier.py
@@ -57,15 +57,11 @@ idxs = np.argsort(preds[0])[::-1][:5]
 for (i, idx) in enumerate(idxs):
     # draw the top prediction on the input image
     if i == 0:
-        text = "Label: {}, {:.2f}%".format(classes[idx],
-            preds[0][idx] * 100)
-        cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-            0.7, (0, 0, 255), 2)
+        text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx] * 100)
+        cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
  
-    # display the predicted label + associated probability to the
-    # console    
-    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
-        classes[idx], preds[0][idx]))
+    # display the predicted label + associated probability to the console    
+    print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1, classes[idx], preds[0][idx]))
  
 # display the output image
 cv2.imshow("Image", image)
